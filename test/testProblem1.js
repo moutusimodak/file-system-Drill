d8cd58824861c86080930abac6ff5265118939ee
moutusi <moutusi.slg01@gmail.com>
2024-09-25 16:47:31 +0530

    modified code by creating my own peomises

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -1,9 +1,23 @@
-import createDir from '../problem1.js';
+import { createDirectory, createRandomFiles, deleteRandomFiles } from '../problem1.js';
 import path from 'path';
 
 
 const RandomDirectory = path.join(process.cwd(), './jsonFiles');
 
-createDir(RandomDirectory, 4);
+createDirectory(RandomDirectory)
+    .then(() => {
+        console.log("directory created successfully");
+        return createRandomFiles(RandomDirectory, 4)
+    })
+    .then((filePaths) => {
+        console.log("files created");
+        return deleteRandomFiles(filePaths)
 
+    })
+    .then((deletedFiles) => {
+        console.log(deletedFiles);
 
+    }).catch((err) => {
+        console.error(err);
+
+    })
--- test/testProblem2.js
+++ test/testProblem2.js
@@ -1,5 +1,38 @@
-import problem2 from '../problem2.js';
+import { readTextFile, convertedToUpperCase, convertedToLowerCase, sortedContent, deleteAllFiles } from '../problem2.js';
+import path from 'path';
 
-let filepath = "../data/lipsum.txt";
 
-problem2(filepath);
+let filepath = path.join('../data/lipsum.txt');
+
+
+readTextFile(filepath)
+    .then((data) => {
+        console.log("file read successfully", data);
+        return convertedToUpperCase(data)
+    })
+
+    .then((upperCaseData) => {
+        console.log("written to filename1.txt", upperCaseData);
+        return convertedToLowerCase("filename1.txt")
+
+    })
+
+    .then((lowerCaseData) => {
+        console.log("written to filename2.txt", lowerCaseData);
+        return sortedContent("filename2.txt");
+    })
+
+    .then((sortedData) => {
+        console.log("written to filename3.txt", sortedData);
+        return deleteAllFiles('filenames.txt');
+    })
+
+    .then((deletedFiles) => {
+        console.log("Deleted", deletedFiles);
+
+    })
+
+    .catch((err) => {
+        console.error("Error occurred : ", err);
+
+    })
